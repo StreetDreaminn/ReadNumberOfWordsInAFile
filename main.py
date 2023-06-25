0ed1d41e7ab08b2364d55a7cec7b5e4c3465c2ab
Franklin <franklin@hotmail.co.uk>
2023-06-25 14:41:06 +0100

    Remove comments.

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-# The purpose of this script is to read a file and generate the total number of times a word appears in the file.
-# It should accept a file as an input and the output should be a number
-
 import fileinput
 
 punctuation = '''!()-[]{};:'"\,<>./?@#$%^&*_~\n'''
